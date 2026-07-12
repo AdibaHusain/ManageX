e6b3cfa3a344afb8d7f5eca4d2b324272e70ce5f
Adiba Husain <adibaknp@gmail.com>
2026-07-12 16:20:05 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ const departmentRoutes = require("./routes/departmentRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const employeeRoutes = require("./routes/employeeRoutes");
 const assetRoutes = require("./routes/assetRoutes");
+const allocationRoutes = require("./routes/allocationRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const maintenanceRoutes = require("./routes/maintenanceRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+const auditRoutes = require("./routes/auditRoutes");
 
 const app = express();   // <-- IMPORTANT
 
@@ -20,6 +25,11 @@ app.use("/api/departments", departmentRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/employees", employeeRoutes);
 app.use("/api/assets", assetRoutes);
+app.use("/api/allocations", allocationRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/maintenance", maintenanceRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/audits", auditRoutes);
 
 const PORT = process.env.PORT || 5000;
 
